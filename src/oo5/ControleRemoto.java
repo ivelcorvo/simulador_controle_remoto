349bb279d65177f12eb1ad5265f020ca458997fd
Levi Alves <levialvesjr@hotmail.com>
2021-02-09 12:25:38 -0300

    inserção + melhorias

--- src/oo5/ControleRemoto.java
+++ src/oo5/ControleRemoto.java
@@ -4,18 +4,17 @@ public class ControleRemoto implements Controlador{
     private int volume;
     private boolean ligado;
     private boolean tocando;
-    
-    // construtor
-    
+    private boolean mudo;
+    //==========================================================================
+    // construtor    
     public ControleRemoto(){
-        this.setVolume(50);
         this.setLigado(false);
         this.setTocando(false);
-        
+        this.setMudo(false);
+        this.setVolume(0);        
     }
-    
+    //==========================================================================
     //geters and setters
-    
     public int getVolume(){
         return this.volume;
     }
@@ -37,99 +36,141 @@ public class ControleRemoto implements Controlador{
         this.tocando = tocando;
     }
     
+    public boolean isMudo(){
+        return this.mudo;
+    }    
+    public void setMudo(boolean mudo){
+        this.mudo = mudo;
+    }    
+    //==========================================================================
     //metodos
-
     @Override
     public void ligar() {
-        this.setLigado(true);
-        System.out.println("|| LIGADO ||\n");
+        if(this.isLigado() == false){            
+            this.setLigado(true);
+            this.setTocando(true);
+            this.setMudo(false); // só por garantia. causo desligue com o mudo ligado
+            this.setVolume(25); // 5 sinais de mais
+            System.out.println("|| LIGANDO ||\n");
+        }else{
+            System.out.println("|| JÀ LIGADO ||\n");
+        }        
     }
-
+    //==========================================================================
     @Override
     public void desligar() {
-        this.setLigado(false);
-        System.out.println("|| DESLIGADO ||\n");
-    }
-
-    @Override
-    public void abrirMenu() {
-        if(this.isLigado()==true){
-            System.out.println("|| ================================== ||");
-            System.out.println("|| LIGADO: "+this.isLigado()+" ||");
-            System.out.print("|| VOLUME: "+this.getVolume()+" ");
-            for(int v=1;v<=this.getVolume();v+=10){
-                System.out.print(" + ");
-            }
-            System.out.println("\n|| ================================== ||");
-            System.out.println("");
+        if(this.isLigado() == true){
+            this.setLigado(false);
+            this.setTocando(false);
+            this.setVolume(0);
+            System.out.println("|| DESLIGANDO ||");
         }else{
-            System.out.println(". . . . .");
+            System.out.println("|| JÁ DESLIGADO ||");
         }
     }
-
+    //==========================================================================
+    @Override
+    public void abrirMenu() {
+        if(this.isLigado() == true){            
+            System.out.print("++-----------------------------++\n"+
+                             "||            MENU             ||\n"+
+                             "++-----------------------------||\n"+
+                             "||  VOLUME: "+this.getVolume() +"\n"+
+                             "|| " );
+            if(this.isMudo() == false){
+                for(int v=1; v<=this.getVolume(); v+=5){
+                    System.out.print(" + ");
+                }
+            }                
+            System.out.print("\n||  MUDO: "+ this.isTocando()+"\n"+                               
+                             "||  ");
+            if(this.isTocando()== true){
+                System.out.print("PLAY\n");
+            }else{
+                System.out.println("PAUSE\n");
+            }
+            System.out.print("++-----------------------------++\n");
+        }else{
+            System.out.println("|| DESLIGADO ||");
+        }
+    }
+    //==========================================================================
     @Override
     public void fecharMenu() {
         if(this.isLigado()==true){
             System.out.println("|| FECHANDO MENU . . . ||");
         }else{
-            System.out.println("|| . . . . . ||");
+            System.out.println("|| DESLIGADO ||");
         }
     }
-
+    //==========================================================================
     @Override
     public void maisVolume() {
-        if(this.isLigado()==true){
-            this.setVolume(this.getVolume()+10);
+        if((this.isLigado() == true) && (this.isMudo() == false)){
+            this.setVolume(this.getVolume() + 5);        
+            System.out.println("|| MAIS VOLUME ||");
+        }else if(this.isMudo() == true){
+            System.out.println("|| MUDO ESTÁ LIGADO ||");
         }else{
-            System.out.println("|| . . . . . ||");
+            System.out.println("|| DESLIGADO ||");
         }
     }
-
+    //==========================================================================
     @Override
     public void menosVolume() {
-        if(this.isLigado()==true){
-            this.setVolume(this.getVolume()-10);
+        if((this.isLigado() == true) && (this.isMudo() == false)){
+            this.setVolume(this.getVolume() - 5);
+            System.out.println("|| MENOS VOLUME ||");
+        }else if(this.isMudo() == true){
+            System.out.println("|| MUDO ESTÁ LIGADO ||");
         }else{
-            System.out.println("|| . . . . . ||");
-        }
+            System.out.println("|| DESLIGADO ||");
+        }    
     }
-
+    //==========================================================================
     @Override
     public void ligarMudo() {
-        if((this.isLigado()==true)&&(this.getVolume()>0)){
+        if((this.isLigado() == true)&&(this.getVolume() > 0) && (this.isMudo() == false)){
             this.setVolume(0);
+            this.setMudo(true);
+            System.out.println("|| MUDO LIGADO ||");
+        }else if(this.getVolume() == 0){
+            System.out.println("|| MUDO JÁ ESTÁ LIGADO ||");
         }else{
-            System.out.println("|| . . . . . ||");
+            System.out.println("|| DESLIGADO ||");
         }
     }
-
+    //==========================================================================
     @Override
     public void desligarMudo() {
-        if((this.isLigado()==true)&&(this.getVolume()==0)){
-            this.setVolume(50);
+        if((this.isLigado() == true)&&(this.getVolume() == 0) && (this.isMudo() == true)){
+            this.setVolume(25);
+            this.setMudo(false);
+            System.out.println("|| MUDO DESLIGADO ||");
+        }else if(this.getVolume() > 0){
+            System.out.println("|| MUDO JÁ ESTÁ DESLIGADO ||");
         }else{
-            System.out.println("|| . . . . . ||");
-        }
+            System.out.println("|| DESLIGADO ||");
+        }    
     }
-
+    //==========================================================================
     @Override
     public void play() {
         if(this.isLigado()==true){
             this.setTocando(true);
-            System.out.println("|| TOCANDO ||");
+            System.out.println("|| PLAY ||");
         }else{
-            System.out.println("|| . . . . . ||");
-        }
-            
+            System.out.println("|| DESLIGADO ||");
+        }            
     }
-
+    //==========================================================================
     @Override
     public void pause() {
         if((this.isLigado()==true)&&(this.isTocando()==true)){
             this.setTocando(false);
             System.out.println("|| PAUSE ||");
         }else{
-            System.out.println("|| . . . . . ||");
+            System.out.println("|| DESLIGADO ||");
         }
     }
     
--- src/oo5/OO5.java
+++ src/oo5/OO5.java
@@ -1,24 +1,60 @@
 package oo5;
+
+import java.util.Scanner;
+
 public class OO5 {
     public static void main(String[] args) {
+        /*
+            as funções só podem ser realizadas com o "aparelho" ligado
+            só pode aumentar ou diminuir o volume com o mudo desligado
+        */
+        int iOpcao, x = 0;
         
-        ControleRemoto c1 = new ControleRemoto();
-        c1.ligar();
-        c1.maisVolume();
-        c1.maisVolume();
-        c1.menosVolume();
-        c1.menosVolume();
-        c1.ligarMudo();
-        c1.desligarMudo();
-        c1.abrirMenu();
-        c1.desligar();
-        c1.ligar();
-        c1.play();
-        c1.pause();
-        c1.abrirMenu();
-       
+        Scanner opcao = new Scanner(System.in);
+        ControleRemoto c = new ControleRemoto();
         
         
-    }
-    
+        System.out.println("++---------------------++\n"+
+                           "||   CONTROLE REMOTO   ||\n"+
+                           "++---------------------++\n"+
+                           "|| [1]  - LIGAR        ||\n"+ 
+                           "|| [2]  - DESLIGAR     ||\n"+
+                           "|| [3]  - PLAY         ||\n"+
+                           "|| [4]  - PAUSE        ||\n"+
+                           "|| [5]  - +VOLUME      ||\n"+
+                           "|| [6]  - -VOLUME      ||\n"+ 
+                           "|| [7]  - LIGAR MUDO   ||\n"+
+                           "|| [8]  - DESLIGAR MUDO||\n"+ 
+                           "|| [9]  - MENU         ||\n"+ 
+                           "|| [10] - SAIR         ||\n"+ 
+                           "++---------------------++");
+        while(x == 0){
+            System.out.print("|| OPÇÃO =>");
+            iOpcao = opcao.nextInt();
+                
+            if((iOpcao < 1)||(iOpcao > 10)){
+                System.out.println("|| OPÇÃO INVÁLIDA ||");
+            }else if(iOpcao == 1){
+                c.ligar();
+            }else if(iOpcao == 2){
+                c.desligar();
+            }else if(iOpcao == 3){
+                c.play();
+            }else if(iOpcao == 4){
+                c.pause();
+            }else if(iOpcao == 5){
+                c.maisVolume();
+            }else if(iOpcao == 6){
+                c.menosVolume();
+            }else if(iOpcao == 7){
+                c.ligarMudo();
+            }else if(iOpcao == 8){
+                c.desligarMudo();
+            }else if(iOpcao == 9){
+                c.abrirMenu();
+            }else if(iOpcao == 10){
+                x = 1; 
+            }
+        }                
+    }    
 }
